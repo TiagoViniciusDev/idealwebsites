a584eaf31eea48c4dfbc0ac5ec317409cc9e4b0c
Tiago Vinicius <tiagoviniciusdev@gmail.com>
2024-03-14 03:57:16 -0300

    button whatsapp

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,4 +1,5 @@
 import './Sobre.css'
+import { FaWhatsappSquare } from "react-icons/fa";
 
 function Sobre() {
   return (
@@ -9,7 +10,10 @@ function Sobre() {
                 <div className='line'></div>
             </div>
             <p>Destaque-se online com um site profissional e atraente! Nossa equipe especializada em criação de sites está pronta para trazer sua visão à vida. Aumente sua presença digital e conquiste mais clientes. Entre em contato conosco hoje e comece a sua jornada rumo ao sucesso online!</p>
-            <a href='https://api.whatsapp.com/send?phone=5586995652410' target='_blank' className='buttonStyle'>Faça seu orçamento</a>
+            <a href='https://api.whatsapp.com/send?phone=5586995652410' target='_blank' className='buttonStyle'>
+              <FaWhatsappSquare />
+              <p>Fale conosco via whatsapp</p>
+            </a>
         </div>
     </section>
   )
